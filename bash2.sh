364d3a3ef05c3937771ef6eacbb88be755d704e5
Sacha MAZOUZ <sacha.mazouz@epitech.eu>
2023-02-07 12:05:59 +0100

    add bonus 2

--- bash2.sh
+++ bash2.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "The arguments are: [" $* "]"
+ARGU=""
+i=1
+
+for ELEMENT in $*; do if [ $i -ne $# ]; then
+ARGU+="$ELEMENT, "
+i=$((i + 1))
+fi;
+done
+ARGU+="$ELEMENT"
+echo "The arguments are: ["$ARGU"]"
